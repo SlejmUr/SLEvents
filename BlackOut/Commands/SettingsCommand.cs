5d36bd764999da19cf350095679a197912436757
SlejmUr <slejmur@protonmail.com>
2024-01-30 12:38:53 +0100

    ref for need to build readme update fixed exiéed isnt getting published (no need to re-upload already existing files)

--- BlackOut/Commands/SettingsCommand.cs
+++ BlackOut/Commands/SettingsCommand.cs
@@ -27,6 +27,7 @@ namespace BlackOut.Commands
                 return false;
             }
             bool IsSet = false;
+            string whatSet = "";
             if (arguments.Count == 1 && !arguments.Contains("list"))
             {
                 foreach (var item in arguments)
@@ -35,27 +36,53 @@ namespace BlackOut.Commands
                     if (arg == "lotus")
                     {
                         Main.Instance.Config.Lotus_Round = !Main.Instance.Config.Lotus_Round;
+                        Main.Instance.Config.Fallout_Round = false;
+                        Main.Instance.Config.JDuff_Round = false;
                         IsSet = true;
+                        if (Main.Instance.Config.Lotus_Round)
+                            whatSet = " - Lotus Round";
+                        else
+                            whatSet = " - Lotus Round Disabled";
                     }
                     if (arg == "fallout")
                     {
                         Main.Instance.Config.Fallout_Round = !Main.Instance.Config.Fallout_Round;
+                        Main.Instance.Config.Lotus_Round = false;
+                        Main.Instance.Config.JDuff_Round = false;
                         IsSet = true;
+                        if (Main.Instance.Config.Fallout_Round)
+                            whatSet = " - Fallout Round";
+                        else
+                            whatSet = " - Fallout Round Disabled";
                     }
                     if (arg == "jduff")
                     {
                         Main.Instance.Config.JDuff_Round = !Main.Instance.Config.JDuff_Round;
+                        Main.Instance.Config.Lotus_Round = false;
+                        Main.Instance.Config.Fallout_Round = false;
                         IsSet = true;
+                        if (Main.Instance.Config.JDuff_Round)
+                            whatSet = " - JDUFF Round";
+                        else
+                            whatSet = " - JDUFF Round Disabled";
+                    }
+                    if (arg == "default")
+                    {
+                        Main.Instance.Config.JDuff_Round = false;
+                        Main.Instance.Config.Lotus_Round = false;
+                        Main.Instance.Config.Fallout_Round = false;
+                        IsSet = true;
+                        whatSet = " - Default Round";
                     }
                 }
 
             }
             if (IsSet)
             {
-                response = "Settings Applied!";
+                response = "Settings Applied!" + whatSet;
                 return true;
             }
-            response = "Example Arguments:\n Lotus or Fallout or JDUFF (can be lowercase)\n list";
+            response = "Example Arguments:\n Lotus or Fallout or JDUFF or Default (can be lowercase)\n list";
             return false;
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# SLEvent
+# SCP:SL Event collection
+SCP:SL Event collection for Kadava's server
+
+Currently featuring:\
+- BlackOut
+- Realistic096
+
+DLLs are can be found in the Actions tab
